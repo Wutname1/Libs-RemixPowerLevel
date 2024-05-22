6f0e0414432aeda603426f1950a04a85d28089f7
wutname1 <jeremy12@gmail.com>
2024-05-22 16:38:21 -0500

    fix character frame code copy

--- main.lua
+++ main.lua
@@ -25,51 +25,54 @@ TooltipDataProcessor.AddTooltipPostCall(
 	end
 )
 
---Hook Character frame
-hooksecurefunc(
-	'PaperDollItemSlotButton_Update',
-	function(button, unit)
-		local slotID = button:GetID()
+local function UpdateItemSlotButton(button, unit)
+	local slotID = button:GetID()
 
-		if slotID >= INVSLOT_FIRST_EQUIPPED and slotID <= INVSLOT_LAST_EQUIPPED then
-			local item
-			if unit == 'player' then
-				item = Item:CreateFromEquipmentSlot(slotID)
-			else
-				local itemID = GetInventoryItemID(unit, slotID)
-				local itemLink = GetInventoryItemLink(unit, slotID)
-				if itemLink or itemID then
-					item = itemLink and Item:CreateFromItemLink(itemLink) or Item:CreateFromItemID(itemID)
-				end
-			end
-
-			if not item or item:IsItemEmpty() then
-				return
-			end
-			if timerunner then
-				-- Create overlay frame if it doesn't exist
-				if not button.ThreadCountOverlay then
-					local overlayFrame = CreateFrame('FRAME', nil, button)
-					overlayFrame:SetAllPoints()
-					overlayFrame:SetFrameLevel(button:GetFrameLevel() + 1)
-					button.ThreadCountOverlay = overlayFrame
-				end
-
-				if string.match(item:GetItemName(), 'Cloak of Infinite Potential') then
-					local c, ThreadCount = {0, 1, 2, 3, 4, 5, 6, 7, 148}, 0
-					for i = 1, 9 do
-						ThreadCount = ThreadCount + C_CurrencyInfo.GetCurrencyInfo(2853 + c[i]).quantity
-					end
-					-- print('Total threads updated: ' .. ThreadCount)
-					if not button.threadCount then
-						button.threadCount = button.ThreadCountOverlay:CreateFontString('$parentItemLevel', 'OVERLAY')
-						button.threadCount:SetFont('fonts/morpheus.ttf', 6, '')
-						button.threadCount:ClearAllPoints()
-						button.threadCount:SetPoint('LEFT', button.ThreadCountOverlay, 'RIGHT', 2, 0)
-					end
-					button.threadCount:SetFormattedText('|cff00FF98Threads:|cffFFFFFF\n' .. comma_value(ThreadCount))
-				end
+	if slotID >= INVSLOT_FIRST_EQUIPPED and slotID <= INVSLOT_LAST_EQUIPPED then
+		local item
+		if unit == 'player' then
+			item = Item:CreateFromEquipmentSlot(slotID)
+		else
+			local itemID = GetInventoryItemID(unit, slotID)
+			local itemLink = GetInventoryItemLink(unit, slotID)
+			if itemLink or itemID then
+				item = itemLink and Item:CreateFromItemLink(itemLink) or Item:CreateFromItemID(itemID)
 			end
 		end
+
+		if not item or item:IsItemEmpty() then
+			return
+		end
+		if timerunner then
+			-- Create overlay frame if it doesn't exist
+			if not button.ThreadCountOverlay then
+				local overlayFrame = CreateFrame('FRAME', nil, button)
+				overlayFrame:SetAllPoints()
+				overlayFrame:SetFrameLevel(button:GetFrameLevel() + 1)
+				button.ThreadCountOverlay = overlayFrame
+			end
+
+			if string.match(item:GetItemName(), 'Cloak of Infinite Potential') then
+				local c, ThreadCount = {0, 1, 2, 3, 4, 5, 6, 7, 148}, 0
+				for i = 1, 9 do
+					ThreadCount = ThreadCount + C_CurrencyInfo.GetCurrencyInfo(2853 + c[i]).quantity
+				end
+				-- print('Total threads updated: ' .. ThreadCount)
+				if not button.threadCount then
+					button.threadCount = button.ThreadCountOverlay:CreateFontString('$parentItemLevel', 'OVERLAY')
+					button.threadCount:SetFont('fonts/arialn.ttf', 13, '')
+					button.threadCount:ClearAllPoints()
+					button.threadCount:SetPoint('LEFT', button.ThreadCountOverlay, 'RIGHT', 2, 0)
+				end
+				button.threadCount:SetFormattedText('|cff00FF98Threads:|cffFFFFFF\n' .. comma_value(ThreadCount))
+			end
+		end
+	end
+end
+
+hooksecurefunc(
+	'PaperDollItemSlotButton_Update',
+	function(button)
+		UpdateItemSlotButton(button, 'player')
 	end
 )
